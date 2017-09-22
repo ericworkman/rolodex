d87df24c3baa5fa9c91aa6088399b9fa9305bd2c
Eric Workman <eworkman@covermymeds.com>
2017-09-22 12:31:02 -0400

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170922025439) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
